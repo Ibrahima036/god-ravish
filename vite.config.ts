f6d617812229628863732cac3446cda1638e6e4f
ibrahima036 <hafismtechnologie@gmail.com>
2025-06-05 21:59:06 +0000

    fix : config vite prise en charge folder pages *html

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/pages/*.html",
+          dest: "pages",
+        },
+      ],
+    }),
+  ],
 });
